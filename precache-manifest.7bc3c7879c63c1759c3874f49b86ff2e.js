394ac177ed5f0613d8665f2e8236176c75a997bf
Dero <106393@gmail.com>
2020-01-10 02:34:57 +0400

    Updates

---
+++ precache-manifest.7bc3c7879c63c1759c3874f49b86ff2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03ca4d60304f2da97b57d6749130d41",
+    "url": "/:Gogn/react-min-quiz.git/index.html"
+  },
+  {
+    "revision": "d49d8dfbb236a420388c",
+    "url": "/:Gogn/react-min-quiz.git/static/css/main.4a6d77b0.chunk.css"
+  },
+  {
+    "revision": "3e62c8d89c959cc10470",
+    "url": "/:Gogn/react-min-quiz.git/static/js/2.2ee336d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/:Gogn/react-min-quiz.git/static/js/2.2ee336d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d49d8dfbb236a420388c",
+    "url": "/:Gogn/react-min-quiz.git/static/js/main.a596d210.chunk.js"
+  },
+  {
+    "revision": "8f3f9b36771f21e0bb67",
+    "url": "/:Gogn/react-min-quiz.git/static/js/runtime-main.6ab673ea.js"
+  }
+]);
