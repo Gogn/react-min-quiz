49aaf609766f1f40e5e77306336d7e1a62003ad7
Dero <106393@gmail.com>
2020-01-13 01:32:02 +0400

    Updates

---
+++ precache-manifest.f4789eab909866fcf75127e86700266c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb807a837e6a830c283aa143d8b7259",
+    "url": "/react-min-quiz/index.html"
+  },
+  {
+    "revision": "730269e86d0e2e232176",
+    "url": "/react-min-quiz/static/css/main.d4d19feb.chunk.css"
+  },
+  {
+    "revision": "7b6e6ddec35f9eeab383",
+    "url": "/react-min-quiz/static/js/2.c952f869.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-min-quiz/static/js/2.c952f869.chunk.js.LICENSE"
+  },
+  {
+    "revision": "730269e86d0e2e232176",
+    "url": "/react-min-quiz/static/js/main.67b967de.chunk.js"
+  },
+  {
+    "revision": "d0fdf7e2f5d517a92ab3",
+    "url": "/react-min-quiz/static/js/runtime-main.6298a333.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-min-quiz/precache-manifest.8c5073512014ce866658626085bbcf3d.js"
+  "/react-min-quiz/precache-manifest.f4789eab909866fcf75127e86700266c.js"
 );
 
 self.addEventListener('message', (event) => {
