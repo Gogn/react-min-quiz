dad54b6c0aa8148c8d4bd1af6c2c30048bf0b28e
Dero <106393@gmail.com>
2020-01-13 19:10:30 +0400

    Updates

---
+++ precache-manifest.84732c680988f0d0445f9e4d70b96e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ec0bdbeec4571590882c214998ece2",
+    "url": "/react-min-quiz/index.html"
+  },
+  {
+    "revision": "50fd6066feaadaf78296",
+    "url": "/react-min-quiz/static/css/main.18127f4a.chunk.css"
+  },
+  {
+    "revision": "376ee4a139b69114cf01",
+    "url": "/react-min-quiz/static/js/2.4a2a0139.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-min-quiz/static/js/2.4a2a0139.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50fd6066feaadaf78296",
+    "url": "/react-min-quiz/static/js/main.3c63d03e.chunk.js"
+  },
+  {
+    "revision": "d0fdf7e2f5d517a92ab3",
+    "url": "/react-min-quiz/static/js/runtime-main.6298a333.js"
+  }
+]);
